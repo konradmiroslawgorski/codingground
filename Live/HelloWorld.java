092bdbc944338b47bf58cb6230dd91ffd93dae3e
konradmiroslawgorski <konrad.gorski@rcafe.pl>
2015-01-08 22:49:24 +0100

    Created by Coding Ground Client

---
+++ Live/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import MyPackage.*;
+
+public class HelloWorld{
+    ArrayList<Integer> places = new ArrayList<Integer>();
+     public static void main(String []args){
+         
+        Live l = new Live(10,10);
+        l.showLiveArea();
+        Envir e = new Envir();
+     }
+}
